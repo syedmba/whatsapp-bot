13413f34e80607f605931d9f5b23bab0d8764fc5
thirddottyboi <syedmba7@connect.hku.hk>
2024-07-09 14:46:06 +0500

    Update bot.py
    + message send working
    - image send not working
    - hardcoded list of numbers

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
+import os
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -12,17 +13,16 @@ driver.get("https://web.whatsapp.com/")
 
 input("Enter anything after scanning the QR Code")
 
-df = pd.read_csv("phonebook.csv")
-phone_list = list(df["Phone"])
+# df = pd.read_csv("phonebook.csv")
+# phone_list = list(df["Phone"])
 
 ################## phone number technique ##################
 
 all_numbers = []
-for number in phone_list:
-    # number = "852" + str(number)
-    all_numbers.append(number)
+# for number in phone_list:
+#     all_numbers.append(number)
 
-numbers_list = ["85298392039", "85298685934", "85256244077"]
+numbers_list = ["+923218401700", "+923032596033", "+923008383072"]
 
 ################## phone number technique ##################
 
@@ -42,23 +42,28 @@ while message != "0":
     
 filepath = input("Enter full filepath: ")
 # filepath = "/Users/syedmuhammadbinasif/Desktop/tree.png/"
+filepath = "/Users/syedmuhammadbinasif/Desktop/swaggy.jpg"
 
 
 for num in numbers_list:
     driver.get(f"https://web.whatsapp.com/send?phone={num}")
     sleep(12)
 
-    attachment_box = driver.find_element(By.XPATH, '//div[@title = "Attach"]')
-    attachment_box.click()
+    if filepath != "":
+        attachment_box = driver.find_element(By.XPATH, '//div[@title = "Attach"]')
+        attachment_box.click()
 
-    sleep(1)
+        sleep(1)
 
-    image_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/span')
-    image_box.click()
-    sleep(10)
-    image_box.send_keys(filepath)
+        image_box = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[2]/li/div')
+        image_box.click()
+        sleep(10)
+        try:
+            image_box.send_keys(os.path.abspath(filepath))
+        except:
+            print("filepath does not exist!")
 
-    sleep(4)
+        sleep(4)
 
     # find the message box
     msg_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
